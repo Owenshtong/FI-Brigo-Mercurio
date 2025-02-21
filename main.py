644dd5d15edb7d0d83e67fc1b73d91cf3bf6566d
Shihao Tong <tongshihaoowen@outlook.com>
2025-02-21 16:13:33 -0500

    1st commitment

---
+++ main.py
@@ -0,0 +1,167 @@
+#### part 1 ###
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.optimize import fsolve
+from scipy.optimize import minimize
+from scipy import optimize
+from scipy.optimize import direct
+from scipy.optimize import differential_evolution
+import part1
+
+# (1)
+info = pd.DataFrame({
+    "maturity": [2, 3, 5, 7, 10, 30],
+    "yield_quote": [.0429, .0434, .0442, .0451, .0460, .0482],
+})
+
+
+T_bill = pd.DataFrame({
+    "maturity": [1/12, 3/12, 6/12, 1],
+    "yield_quote": [.0433, .0429, .0422, .0414]})
+T_bill.set_index('maturity', inplace=True)
+T_bill["P"] = 1 - T_bill.yield_quote * T_bill.index
+
+# Solve for discount factor
+p = T_bill.P
+for t in range(6):
+    fun = part1._root_equation(p)
+    T = info.maturity[t]
+    c = info.yield_quote[t]
+    pT = fsolve(fun, args = (c,T), x0 = 0.9)[0]
+    p[T] = pT
+
+# Turn into zcy
+y = (-1) * np.log(p) / p.index
+
+
+#(2)
+nss_vect = np.vectorize(part1.nss)
+opt_result = minimize(lambda x: sum((nss_vect(y.index,*x) - y)**2),
+                      # x0 =[0.057, -0.013, -0.014, -0.028, 1.175, 5.15],
+                      x0=[0.05, -0.01, -0.01, -0.03, 1.15, 5.1],
+                      method='L-BFGS-B')
+x_op = opt_result["x"]
+
+t = np.linspace(0.001, 35, 100)
+nss_fit = nss_vect(t ,*x_op)
+
+fig, ax = plt.subplots(figsize=(9, 5), dpi = 100)
+ax.scatter(y.index, y * 100, color = "dodgerblue", marker="D",label = "Observed",s = 20)
+ax.plot(t, nss_fit * 100, color = "red", label = "NSS Fitted")
+plt.title("Smoothed NSS Curve", fontname="Times New Roman")
+plt.xlabel("Maturity (t)")
+plt.ylabel("y(t)%")
+plt.legend(facecolor='none', edgecolor='none')
+plt.gcf()
+plt.savefig("nss.pdf")
+plt.show()
+
+# (3)
+# market quoted vol
+cap_quoted_vol = pd.DataFrame({
+    0.95: [0.157, 0.176, 0.175, 0.168, 0.155, 0.142],
+    1: [0.152, 0.164, 0.161, 0.156, 0.148, 0.126],
+    1.05: [0.146, 0.157, 0.158, 0.152, 0.140, 0.121],
+    "Maturity": [1,3,5,7,10,20]
+})
+
+
+# Solve the K_f for each maturity
+K_atm_vect = np.vectorize(part1.K_atm, excluded=["nss_para", "tau"])
+K_atm = K_atm_vect(cap_quoted_vol.Maturity, nss_para = x_op)
+
+
+# strike with maturity
+strike = pd.DataFrame(
+    {
+        "Maturity": [1,3,5,7,10,20],
+        "K_atm": K_atm,
+    }
+)
+cap_quoted_vol = cap_quoted_vol.melt(id_vars=['Maturity'], value_vars=[0.95, 1, 1.05],
+                    var_name='Strike_times', value_name='IV_market')
+
+cap_quoted_vol = pd.merge(cap_quoted_vol, strike,  how='left', on = "Maturity")
+cap_quoted_vol.K_atm = cap_quoted_vol.K_atm * cap_quoted_vol.Strike_times
+cap_quoted_vol["K_atm_zcb"]  = 1 / (1 + 0.25 * cap_quoted_vol.K_atm)
+
+bms_cap_vect = np.vectorize(part1.bms_cap, excluded=["pack", "tau", "M"])
+cap_quoted_vol["BlackPrice"] = bms_cap_vect(cap_quoted_vol.K_atm, cap_quoted_vol.Maturity, cap_quoted_vol.IV_market, pack = x_op)
+
+
+# # Target function
+BM_cap_vect = np.vectorize(part1.BrigoMercurio_cap, excluded=["t", "kappa", "theta", "sigma", "rt", "gamma", "x0","pack_nss" ,"tau"])
+def sse(BM_para):
+    cap_BM = BM_cap_vect(
+        t = 0.003,
+        K = cap_quoted_vol.K_atm_zcb,
+        T = cap_quoted_vol.Maturity,
+        kappa = BM_para[0],
+        theta = BM_para[1],
+        sigma = BM_para[2],
+        rt = BM_para[3],
+        gamma = BM_para[4],
+        x0 = 0,
+        pack_nss = x_op
+    )
+    market_premium = cap_quoted_vol.BlackPrice
+    loss = sum((cap_quoted_vol.Maturity * (cap_BM - market_premium)/(market_premium))**2)
+    # loss = sum((cap_BM - market_premium) ** 2)
+    print([BM_para, loss])
+    return loss
+
+# "kappa", "theta", "sigma", "rt", "gamma",
+bunds = [(0.05,1),(0.001,0.2),(0.01,0.4),(0.01, 0.1),(0.01,0.5)]
+opt_result = minimize(sse,
+                      x0= [0.01323959, 0.09478633, 0.0479164 , 0.04820152, 0.06845098] ,
+                      method='L-BFGS-B',
+                      bounds=bunds,
+                      options={'maxiter': 10000}
+                      )
+pack_BM = opt_result["x"]
+
+pack_BM = [0.01323959, 0.09478633, 0.0479164 , 0.04820152, 0.06845098]
+# [0.01323959, 0.09478633, 0.0479164 , 0.04820152, 0.06845098]  converges well on sse%
+p_bm = BM_cap_vect(
+    kappa=pack_BM[0],
+    theta=pack_BM[1],
+    sigma=pack_BM[2],
+    rt=pack_BM[3],
+    gamma=pack_BM[4],
+    x0=0, K=cap_quoted_vol.K_atm_zcb, T=cap_quoted_vol.Maturity, t=0.003, pack_nss=x_op
+)
+sse_percent = sse(pack_BM)
+cap_quoted_vol["BMPrice"] = p_bm
+
+# Back out the IV
+def f(sigma, K ,T ,pack, price):
+    return part1.bms_cap(K ,T ,sigma, pack) - price
+def root(K, T, price, pack):
+   return fsolve(f, args=(K, T, pack, price), x0=0.1)
+root_vect = np.vectorize(root, excluded=["pack"])
+cap_quoted_vol["BM_IV"] = root_vect(
+    K = cap_quoted_vol.K_atm,
+    T = cap_quoted_vol.Maturity,
+    price = cap_quoted_vol.BMPrice,
+    pack = x_op
+)
+
+
+
+# Plot
+M = [0.95, 1, 1.05]
+fig, ax = plt.subplots(nrows=3, ncols=2, figsize=(10, 12))
+for i in range(3):
+    atm = cap_quoted_vol[cap_quoted_vol['Strike_times'] == M[i]]
+    axj1 = ax[i,0]
+    axj2 = ax[i,1]
+    axj1.plot(atm.Maturity, atm.BlackPrice, label = "Observed")
+    axj1.plot(atm.Maturity, atm.BMPrice, label="Modeled")
+    axj1.set_title("Brigo-Mercurio Price v.s. Black Price")
+
+    axj2.plot(atm.Maturity, atm.IV_market, label="Observed")
+    axj2.plot(atm.Maturity, atm.BM_IV, label="Modeled")
+    axj2.set_title("Brigo-Mercurio IV v.s. Quoted IV")
+plt.legend(facecolor='none', edgecolor='none')
+plt.show()
---
+++ part1.py
@@ -0,0 +1,208 @@
+# Functions for Part1 #
+import numpy as np
+from scipy.stats import norm
+from scipy.stats import ncx2
+
+
+# (1)
+def _P_interpolation(t, t0, t1, p0, p1):
+    """
+    :param t: t \in [t0,t1] to be interpolated
+    :param t0: right end on t
+    :param t1: left end on t
+    :param p0: fun value at t0
+    :param p1: fun value at t1
+    :return: interpolated p(t)
+    """
+    return (t1 - t0)**(-1) * (p0 * (t1 - t) + p1 * (t -t0))
+
+def _root_equation(p_v):
+    """
+    :param p_v: pd.series of discount factor
+    :return: a function of target discount
+    """
+
+    T0 = p_v.index.max() # Interpolate after this envolves the root
+    P0 = p_v[T0]
+    def _to_be_solved(x,c,T):
+        """
+        :param x: the discount factor to be solved
+        :param c: coupon rate
+        :param T: maturity of the coupon bearing bond
+        """
+        coupon = 100 * c/2
+        acc = 0
+        for i in range(int(T0/0.5)): # number of semi-years
+            t_year = (1+i) * 0.5
+            lt = max(p_v.index[p_v.index < t_year])
+            ut = min(p_v.index[p_v.index >= t_year])
+            lp = p_v[lt]
+            up = p_v[ut]
+            acc += coupon * _P_interpolation(t_year, lt, ut, lp, up)
+        for i in range(int(T0/0.5), int(T/0.5)):
+            t_year = (1 + i) * 0.5
+            acc += coupon * _P_interpolation(t_year, T0, T, P0, x)
+        acc += 100 * x
+        return acc - 100
+    return  _to_be_solved
+
+# (2)
+def nss(t,a,b,c,d, tau, theta):
+    f1 = 1
+    f2 = (1 - np.exp(-t / tau))/(t/tau)
+    f3 = f2 - np.exp(-t/tau)
+    f4 = (1 - np.exp(-t / theta))/(t/theta)- np.exp(-t/theta)
+    return a*f1 + b*f2 + c*f3 + d*f4
+
+
+# (3)
+# P(0,t) ZCB from the nss curve
+def P(t,a,b,c,d, tau, theta):
+    return np.exp(-1 * nss(t,a,b,c,d, tau, theta) * t)
+
+# proportional forward rate, t2 > t1
+def F(Pt1, Pt2, t1, t2):
+    return ((Pt1 / Pt2) - 1) * (1 / (t2 - t1))
+
+# Black formula for market cap
+def bms_cap(K ,T ,sigma, pack, tau = .25, M = 1):
+    """
+    :param K: strike
+    :param T: ANNUAL Maturity
+    :param sigma: combined IV
+    :param pack: the NSS parameters
+    :param tau: Settlement period
+    :param M: Nominal amount
+    :return: The market cap price
+    """
+    n = T / tau
+
+    cap = 0
+    for j in range(2, int(n) + 1):
+        # example: 1 year cap has 3 settlement. Start with 2.
+        Pj_1 = P(tau * (j-1), *pack)
+        Pj = P(tau * j, *pack)
+        Fj_1 = F(Pj_1, Pj, tau * (j-1), tau * j)
+        vj = sigma * np.sqrt(tau * (j-1))
+
+        d1 = (1/vj) * (np.log(Fj_1 / K) + vj**2 / 2)
+        d2 = d1 - vj
+
+        cap += M * tau * Pj * (Fj_1 * norm.cdf(d1) - K * norm.cdf(d2))
+    return cap
+
+#instantaneous forward rate from NSS
+def nss_f(t,a,b,c,d, tau, theta):
+    return a + b*np.exp(-1 * t/tau) + \
+        c* (t/tau) * np.exp(-1 * t/tau) + d * t/theta * np.exp(-1 * t/theta)
+
+# Cap ATM K level
+def K_atm(T, nss_para, tau = 0.25):
+    # nss_para is the list of parameters of NSS curve in order of a,b,c,d,tau, theta
+    n = T/tau
+    acc = 0
+    for i in range(2, int(n + 1)):
+        acc += tau * P(tau * i, *nss_para)
+    return (1 - P(T, *nss_para)) / acc
+
+
+### BrigoMercurio universe ###
+# The single put written on zcb
+def BM_put_ZCB(t, K, Tg, T, kappa, theta, sigma, rt, gamma, x0, pack_nss):
+    # Market price of P
+    Pmt, PmTg, PmT =  P(t, *pack_nss), P(Tg, *pack_nss), P(T, *pack_nss)
+    ft = nss_f(t,*pack_nss)
+
+    # constants
+    PbmtT = P_bm(t, T, Pmt, PmT, ft, kappa, theta, sigma, rt, gamma, x0)
+    PbmtTg = P_bm(t, Tg, Pmt, PmTg, ft, kappa, theta, sigma, rt, gamma, x0)
+
+    # a & b
+    a = lambda t, tt: a_cir(t, tt, kappa, theta, sigma, gamma)
+    b = lambda t, tt: b_cir(t, tt, kappa, theta)
+
+    acir_TgT, acir_0T, acir_0Tg, bcir_TgT, bcir_0T, bcir_0Tg = a(Tg, T), a(0,T), a(0,Tg), b(Tg, T), b(0,T), b(0,Tg)
+
+    # greeks
+    rhat = (1 / bcir_TgT) * (
+            np.log(acir_TgT/K) - np.log((PmTg * acir_0T * np.exp(-1 * bcir_0T * x0)) / (PmT * acir_0Tg * np.exp(-1 * bcir_0Tg* x0)))
+    )
+    phi = 2 * gamma / (sigma**2 * (np.exp(gamma * (T - t)) - 1))
+    psi = (gamma + kappa)/sigma**2
+    xi = phi + psi + bcir_TgT
+    shift_t = shift(t, ft, kappa, theta, gamma, x0)
+
+    # print(rhat, phi, psi, shift_t)
+
+    # probabilities
+    chi1 = ncx2.cdf(2 * xi * rhat,
+                    df = 4 * kappa * theta / sigma**2,
+                    nc = (2 * phi**2 * (rt - shift_t) * np.exp(gamma * (Tg - t)))/xi)
+    chi2 = ncx2.cdf(2 * (phi + psi) * rhat,
+                    df=4 * kappa * theta / sigma ** 2,
+                    nc=(2 * phi ** 2 * (rt - shift_t) * np.exp(gamma * (Tg - t))) / (phi + psi))
+
+    # Put premium
+    g = K * PbmtTg * (1 - chi2) - PbmtT * (1 - chi1)
+    return g * (1 / K)
+
+
+def BrigoMercurio_cap(t, K, T,
+                      kappa, theta, sigma, rt, gamma, x0,
+                      pack_nss ,
+                      tau = 0.25):
+    """
+    :param tau: reset period
+    :param t: o.oo3 (1 day or around)
+    :param K: The strike for all puts
+    :param T: Maturity of the CAP
+    :return: Price of the cap
+    """
+    n = T/tau
+    acc = 0
+    for j in range(2, int(n) + 1):
+        Tg, T = (j-1) * tau, j * tau
+        put_j = BM_put_ZCB(t, K, Tg, T,   kappa, theta, sigma, rt, gamma, x0, pack_nss)
+        acc += put_j
+    return acc
+
+
+def a_cir(t1, t2, kappa, theta, sigma, gamma):
+    numer = 2 * gamma * np.exp(((kappa + gamma)/2) * (t2 - t1))
+    denom = (kappa + gamma) * (np.exp(gamma * (t2 - t1)) - 1) + 2 * gamma
+    return  (numer / denom) ** (2 * kappa * theta / sigma**2)
+
+def b_cir(t1, t2, kappa, gamma):
+    numer = 2 * (np.exp(gamma * (t2 - t1)) - 1)                                  ## Check -1 inside or outside
+    denom = (kappa + gamma) * (np.exp(gamma * (t2 - t1)) - 1) + 2 * gamma
+    return numer / denom
+
+def a_bm(t1, t2, Pmt1, Pmt2, ft, kappa, theta, sigma, gamma, x0):
+    a = lambda t, tt: a_cir(t,tt, kappa, theta, sigma, gamma)
+    b = lambda t, tt: b_cir(t,tt, kappa, theta)
+
+    numerator   = Pmt2 * a(0,t1) * np.exp(-1 * b(0,t1) * x0) * a(t1,t2) * np.exp(b(t1,t2) * ft)
+    denominator = Pmt1 * a(0,t2) * np.exp(-1 * b(0,t2) * x0)
+
+    return numerator / denominator
+
+def b_bm(t1, t2, kappa, gamma):
+    return b_cir(t1, t2, kappa, gamma)
+
+def P_bm(t1, t2, Pmt1, Pmt2, ft, kappa, theta, sigma, rt, gamma, x0):
+    a = a_bm(t1, t2, Pmt1, Pmt2, ft, kappa, theta, sigma, gamma, x0)
+    b = b_bm(t1, t2, kappa, gamma)
+    return a * np.exp(-1 * b * rt)
+
+def shift(t, fm_0t, kappa, theta, gamma, x0):
+    # fm_0t is market instantaneous forward rate. From nss or other smoothed yield
+    denominator = (kappa + gamma) * (np.exp(gamma * t) - 1) + 2 * gamma
+    term1 = fm_0t
+    term2 = kappa * theta * (np.exp(gamma * t)-1) / denominator
+    term3 = x0 * 4 * gamma**2 * np.exp(gamma * t) / denominator**2
+    return term1 + term2 + term3
+
+
+
+
+
